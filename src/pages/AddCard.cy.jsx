a536add36cf0944ecfd9b40ba609c66d87126502
Tonia Dias <tdsouzza@gmail.com>
2025-08-12 20:23:09 -0300

    simulando um bug

--- src/pages/AddCard.cy.jsx
+++ src/pages/AddCard.cy.jsx
@@ -68,7 +68,7 @@ describe('<AddCard />', () => {
 
     cy.get('.notice-success')
       .should('be.visible')
-      .and('have.text', 'Cartão cadastrado com sucesso!');
+      .and('have.text', 'Cartão cadastrado!');
   });
 
   it('valida nome do titular com menos de 2 caracteres', () => {
